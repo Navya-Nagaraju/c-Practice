3bf13de857782d4f5593144aabf7dadff82dae30
Navya Nagaraju <navyanagaraju05@gmail.com>
2024-05-08 11:15:29 -0400

    Committed OOPSPractice codes

---
+++ OOPSConcepts/Defining.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts
+{
+    internal class Defining
+    {
+        //classes have a name, unique within the namespace
+        public class Book
+        {
+            //classes have member variables or fields to hold data
+            string _name;
+            string _author;
+            int _pagecount;
+
+            //classes have one or more constructors
+            public Book(string name, string author, int pages)
+            {
+                _name = name;
+                _author = author;
+                _pagecount = pages;
+            }
+
+            //Methods are used to operate in the class and data
+            public string GetDescription()
+            {
+                return $"{_name} by {_author}";
+
+            }
+
+
+        }
+    }
+}
---
+++ OOPSConcepts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OOPSConcepts.Defining;
+
+namespace OOPSConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new object instances using new operator
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+            Book b2 = new Book("The grapes of wrath", "John stein beck", 925);
+
+            //call a method on the object
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
+
+            //try to set one of the properties 
+            //this will result in an error
+
+        }
+    }
+}
