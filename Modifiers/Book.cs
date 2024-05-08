e07d229a533e69b13730a646d450611e913d8967
Navya Nagaraju <navyanagaraju05@gmail.com>
2024-05-08 11:03:39 -0400

    first commit

---
+++ Modifiers/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifiers
+{
+    //access modifiers control how properties and methods are accessed
+    class Book
+    {
+        //public members and methods can be accessed by any other code
+        public string _name;
+
+        //protected members can only be accessed by the class or a derived class  
+        protected string _author;
+
+        //private is the default and can only be accessed by the code
+        private int _pagecount;
+
+        public Book (string name, string author, int pages)
+        {
+            _name = name;
+            _author = author;   
+            _pagecount = pages;
+
+        }
+
+        public string GetDescription()
+        {
+            return $"{_name} by {_author} and {_pagecount}";
+
+        }
+
+        /*protected string GetDescription()
+        {
+            return $"{_name} by {_author}";
+
+        }*/
+        /* private string GetDescription()
+         {
+             return $"{_name} by {_author}";
+
+         }
+ */
+        //member variables can be accessedvia methods
+        public string GetName()
+        {
+            return _name ;
+        }
+        
+        public void SetName(string s)
+        {
+            _name = s ; 
+        }
+        public void SetAuthor(string s)
+        {
+            _author = s ;
+        }
+        public void Setpagecount (int c)
+        {
+            _pagecount = c ;
+        }
+
+    }
+}
---
+++ Modifiers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                //create new object instances using new operator
+                Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+                Book b2 = new Book("The grapes of wrath", "John stein beck", 925);
+
+            //call a method on the object
+           /* b1._name = "Cat in the hat";
+            Console.WriteLine(b1.GetDescription());
+*/
+            //set data using functions instead of accessing fields directly
+            b1.SetName("Grapes of wrath");
+            b1.SetAuthor("John steinbeck");
+            b1.Setpagecount(925);
+            Console.WriteLine(b1.GetDescription());
+           
+
+                
+
+            
+        }
+    }
+    
+}
